6f885dce9e5c0e8400b30f630ba9c697702a01e4
MtheEPIC <mtheepic1423@protonmail.com>
2020-11-15 22:59:03 +0200

    changed print and now asks for value of X once per run

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
 {
 	double x, ans;
 	
-	printf("1. Enter double\n");
+	printf("Enter a real number\n");
 	scanf("%lf", &x);
 	
 	double a, b;
@@ -13,21 +13,17 @@ int main()
 	b = Power(x, 3);
 	
 	ans = sub(add(a, b), 2);
-	printf("%0.4lf\n", ans);
+	printf("f(%0.4lf)=e^x+x^3-2=%0.4lf\n", x, ans);
 	
-	printf("2. Enter double\n");
-	scanf("%lf", &x);
 	a = mul(3, x);
 	b = mul(2, Power(x, 2));
 	ans = add(a, b);
-	printf("%0.4lf\n", ans);
+	printf("f(%0.4lf)=3x+2x^2=%0.4lf\n", x, ans);
 	
-	printf("3. Enter double\n");
-	scanf("%lf", &x);
 	a = mul(4, Power(x, 3));
 	b = mul(2, x);
 	ans = sub(div(a, 5), b);
-	printf("%0.4lf\n", ans);
+	printf("f(%0.4lf)=(4x^3)/5-2x)=%0.4lf\n", x, ans);
    
    return 0;
 }
